811a856a03afa83fcf80024cf3e4516d9aac8bee
bloodlordik <bloodlordik@yandex.ru>
2023-03-05 11:49:08 +0700

    Add tests for two sum problem

---
+++ src/main/kotlin/two_sum/main.kt
@@ -0,0 +1,7 @@
+package two_sum
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+            return intArrayOf(0,5)
+    }
+}
---
+++ src/test/kotlin/two_sum/SolutionTest.kt
@@ -0,0 +1,40 @@
+package two_sum
+
+
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class SolutionTest{
+    private val instance = Solution()
+
+    @Test
+    fun `first simple test`(){
+        val input = intArrayOf(2,7,11,15)
+        val target = 9
+        val expect = intArrayOf(0, 1)
+
+        val actual = instance.twoSum(nums = input, target = target)
+        assertContentEquals(expect, actual)
+    }
+    @Test
+    fun `first second test`(){
+        val input = intArrayOf(3,2,4)
+        val target = 6
+        val expect = intArrayOf(1,2)
+
+        val actual = instance.twoSum(nums = input, target = target)
+        assertContentEquals(expect, actual)
+
+    }
+    @Test
+    fun `third simple test`(){
+        val input = intArrayOf(3,3)
+        val target = 6
+        val expect = intArrayOf(0, 1)
+
+        val actual = instance.twoSum(nums = input, target = target)
+        assertContentEquals(expect, actual)
+
+    }
+}
