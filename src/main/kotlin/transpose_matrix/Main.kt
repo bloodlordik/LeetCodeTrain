595c716b0f544c7196b4f759b8c9ccc342851c14
bloodlordik <bloodlordik@yandex.ru>
2023-03-06 10:33:02 +0700

    Add tranform matrix solution

--- src/main/kotlin/transpose_matrix/Main.kt
+++ src/main/kotlin/transpose_matrix/Main.kt
@@ -2,6 +2,22 @@ package transpose_matrix
 
 class Solution {
     fun transpose(matrix: Array<IntArray>): Array<IntArray> {
-        return arrayOf(intArrayOf(1,4,8),intArrayOf(2,5,8),intArrayOf(3,6,9))
+        val size = matrix[0].size
+        var result = Array<IntArray>(size){index ->
+            var newList = mutableListOf<Int>()
+
+               for (i in matrix){
+                  newList.add(i[index])
+               }
+          return@Array newList.toIntArray()
+        }
+       return result
     }
 }
+
+/*
+Для того, чтобы транспонировать матрицу достаточно поменять местами столбцы и строки в ней по формуле:
+A_{ij} ^T = A_{ji}
+
+
+ */
